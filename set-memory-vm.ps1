893e379bf924d2fb898aa37a53ce00c5fd08601b
pauldiee <paul@vandieen.com>
2019-06-06 16:49:26 +0200

    small-citrix related scripts

--- set-memory-vm.ps1
+++ set-memory-vm.ps1
@@ -1,18 +1,21 @@
-$offlinevms = (Get-VMHost "srv-mtw-esxw-41*" | Get-VM | Where-Object {$_.PowerState -eq "PoweredOff"})
-
-foreach ($vm in $offlinevms){
-    Set-VM -VM $vm -MemoryGB 7 -Confirm:$false | Out-Null
-    Write-Host $vm set to 7GB Memory -ForegroundColor Green
+while (Get-VM "VD071*" | Where-object {$_.powerstate -eq "poweredon"}) { 
+    $offlinevms = (Get-VM "VD071*"| Where-Object {$_.PowerState -eq "PoweredOff"})
+    foreach ($vm in $offlinevms){
+        if ((get-vm $vm |Where-Object {$_.MemoryGB -eq "8"})){
+            Set-VM -VM $vm -MemoryGB 7 -Confirm:$false | Out-Null
+            Write-Host $vm set to 7GB Memory -ForegroundColor Green
+        } else{
+        Write-Host $vm already set to 7GB Memory -ForegroundColor Cyan
+        }
+    }    
+Start-Sleep -s 60
 }
-
-$templatevm = ""
-$templatevm = (Get-Cluster | Get-VMHost | Get-Template -Name "VD_Template_W7x64_srv*" | Sort-Object Name)
-foreach ($vm in $templatevm){
-    Set-Template $vm -ToVM | Out-Null
-    Write-Host $vm set from Template to VM -ForegroundColor Green
-    $next = (Get-VM $vm)
-    Set-VM $next -MemoryGB 7 -Confirm:$false | Out-Null
-    Write-Host $next set to 7GB Memory -ForegroundColor Green
-    Set-VM -VM $next -ToTemplate -Confirm:$false | Out-Null
-    Write-Host $next set from VM to Template -ForegroundColor Green
+$offlinevms = (Get-VM "VD071*"| Where-Object {$_.PowerState -eq "PoweredOff"})
+foreach ($vm in $offlinevms){
+    if ((get-vm $vm |Where-Object {$_.MemoryGB -eq "8"})){
+        Set-VM -VM $vm -MemoryGB 7 -Confirm:$false | Out-Null
+        Write-Host $vm set to 7GB Memory -ForegroundColor Green
+    } else{
+    Write-Host $vm already set to 7GB Memory -ForegroundColor Cyan
+    }
 }
