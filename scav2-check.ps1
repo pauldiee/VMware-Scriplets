520cca9c605b927f212b0f109cf20604b5eff1d8
pauldiee <paul@vandieen.com>
2019-06-22 16:19:08 +0200

    scav2 final release

--- scav2-check.ps1
+++ scav2-check.ps1
@@ -30,11 +30,10 @@ Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true -Confirm:$false |
 if ($global:DefaultVIServers.Count -gt 0) {Disconnect-VIServer * -Confirm:$false}
 Write-Host -BackgroundColor Yellow -ForegroundColor Black "Please fill in all information for vCenter Connection!"
 $vcenter = Read-Host "Enter vCenter name"
-$domain = Read-Host "Enter local domain name"
-$username = Read-Host "Enter Username"
-$password = Read-Host "Enter Password" -AsSecureString
+#$domain = Read-Host "Enter local domain name"
+$vCenterCredential = Get-Credential
 
-Connect-VIServer $vcenter"."$domain -User $username -Password $password -Force -ErrorAction Stop | Out-Null
+Connect-VIServer $vcenter -Credential $vCenterCredential -Force -ErrorAction Stop | Out-Null
 Write-host -ForegroundColor Green "Connected to vCenter server: $($global:DefaultVIServer.Name)"
 
 #Check if SCAv2 is enabled
@@ -43,13 +42,29 @@ foreach ($esxhost in $allhosts){
     if (Get-VMHost $esxhost | Get-VMHostVersion | Where-Object {$_.UpdateRelease -eq "ESXi 6.7 U2"}){
         if (Get-VMHost $esxhost | Get-AdvancedSetting VMkernel.Boot.hyperthreadingMitigation |Where-Object {$_.Value -eq $true}){
             Write-Host HT Mitigation is enabled on $esxhost -ForegroundColor Green
-            if (Get-VMHost $esxhost | Get-AdvancedSetting VMkernel.Boot.hyperthreadingMitigationIntraVM |Where-Object {$_.Value -eq $false}){
-                Write-Host SCAv2 is enabled on $esxhost -ForegroundColor Green
-            } else{
-                Write-Host SCAv2 is NOT enabled on $esxhost -ForegroundColor Cyan
-            }
         } else{
             Write-Host HT Mitigation is NOT enabled on $esxhost -ForegroundColor Cyan
+        }
+        if (Get-VMHost $esxhost | Get-AdvancedSetting VMkernel.Boot.hyperthreadingMitigationIntraVM |Where-Object {$_.Value -eq $false}){
+            Write-Host SCAv2 is enabled on $esxhost -ForegroundColor Green
+            $continue1 = Read-Host "Do you want to disable SCAv2 Mitigation? (y/n)"
+            if ($continue1 -eq "y"){
+                Get-VMHost $esxhost | Get-AdvancedSetting VMkernel.Boot.hyperthreadingMitigationIntraVM | Set-AdvancedSetting -Value $true -Confirm:$false | Out-Null
+                Get-VMHost $esxhost | Get-AdvancedSetting VMkernel.Boot.hyperthreadingMitigation | Set-AdvancedSetting -Value $false -Confirm:$false | Out-Null
+                Write-Host Disabled SCAv2 on $esxhost! Please REBOOT $esxhost! -ForegroundColor Green
+            }else{
+                Write-Host Done nothing to current settings -ForegroundColor Cyan
+            }
+        } else{
+            Write-Host SCAv2 is NOT enabled on $esxhost -ForegroundColor Cyan
+            $continue2 = Read-Host "Do you want to enable SCAv2 Mitigation? (y/n)"
+            if ($continue2 -eq "y"){
+                Get-VMHost $esxhost | Get-AdvancedSetting VMkernel.Boot.hyperthreadingMitigationIntraVM | Set-AdvancedSetting -Value $false -Confirm:$false | Out-Null
+                Get-VMHost $esxhost | Get-AdvancedSetting VMkernel.Boot.hyperthreadingMitigation | Set-AdvancedSetting -Value $true -Confirm:$false | Out-Null
+                Write-Host Enabled SCAv2 on $esxhost! Please REBOOT $esxhost! -ForegroundColor Green
+            }else{
+                Write-Host Done nothing to current settings -ForegroundColor Cyan
+            }
         }
     }else {
         Write-Host Version of $esxhost is not a minumum of 6.7 Update 2 -ForegroundColor Cyan
