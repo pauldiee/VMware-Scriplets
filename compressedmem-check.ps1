bdce1ea8ac0537ad1344a1f77ef144a9ed243597
pauldiee <paul@vandieen.com>
2019-07-18 16:32:03 +0200

    initial

---
+++ compressedmem-check.ps1
@@ -0,0 +1,33 @@
+$WorkingDir = split-path -parent $PSCommandPath
+Set-Location -Path $WorkingDir
+
+$vCenter = Read-Host -prompt 'Enter FQDN / IP address of vCenter'
+Write-Host 'vCenter:' $vCenter ''
+
+# Connect to vCenter with $vCenter variable value
+Connect-VIServer -Server $vCenter -Force
+
+#Create report vm's with compressed mem
+$report = @()
+foreach($vm in Get-View -ViewType Virtualmachine){
+ 
+    $vms = "" | Select-Object VMName,VMHost,Compressed,Ballooned,Swapped
+ 
+    $vms.VMName = $vm.Name
+    $vms.VMHost = Get-View -Id $vm.Runtime.Host -property Name | select -ExpandProperty Name
+    $vms.Compressed = $vm.Summary.QuickStats.CompressedMemory
+    $vms.Ballooned = $vm.Summary.QuickStats.BalloonedMemory
+    $vms.Swapped = $vm.Summary.QuickStats.SwappedMemory
+    $Report += $vms
+}
+
+$output = $Report | Sort-Object -Property Compressed | Where-Object {$_.Compressed -ne "0"}
+
+#Report aanmaken en wegschrijven voor logging
+$Report = $output | Select VMName,Compressed | ConvertTo-Html -Head $Header -Property VMName,Compressed -PreContent "<p><h2>VM Compressed Memory Report - $($vcenter)</h2></p><br>"
+$reportname = "$($vcenter)-compressedmemvm.html"
+$Report | Out-File "\$($reportname)"
+
+# Close connection to active vCenter
+Disconnect-VIServer $vCenter -Confirm:$false
+Write-Host 'Connection closed to' $vCenter
